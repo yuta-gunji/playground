d5a4358050347e07dd90cf0c326656d50b051055
Yuta Gunji <yuta.g05@gmail.com>
2020-06-28 22:26:56 +0900

    Practice

---
+++ vue-practice/template/main.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: '#app',
+  data: {
+    rawHtml: '<span style="color: red">This should be red.</span>',
+  }
+})
+
+new Vue({
+  el: '#app-2',
+  data: {
+    seen: true,
+    notSeen: false
+  }
+})
